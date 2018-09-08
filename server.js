26f70c57e6ecea3170bc5905d9fd6b9dd744f6b9
OluJava <oluwolemoses@gmail.com>
2018-09-08 15:50:38 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+
+
+const express= require('express'),
+path = require('path');
+
+const app =express();
+
+
+app.use(express.static('./dist/myapp'));
+
+app.get('/*', (req,res)=>{
+
+res.sendFile(path.join(__dirname,'/dist/myapp/index.html'));
+
+});
+
+app.listen(process.env.PORT || 8080, ()=>{
+console.log('Server started');
+})
